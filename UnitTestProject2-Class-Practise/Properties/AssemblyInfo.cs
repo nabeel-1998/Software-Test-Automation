71e0d8d0191b1ba464d2323734b9005140ff486c
Nabeel <Nabeel@DESKTOP-KJNHHLE>
2025-03-08 11:24:17 +0500

    Add project files.

---
+++ UnitTestProject2-Class-Practise/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2-Class-Practise")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2-Class-Practise")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f2d75db-d9dc-4ff7-8753-3c782ebf1695")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2-Class-Practise/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnitTestProject2_Class_Practise
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        //Attributes
+        //assigning Owner (not more than 1)
+        [Owner("Nabeel")]
+        //Desciription
+        [Description("Test is demo Test")]
+        //WorkItem Different Tasks jo kisi kam k soorat me hon like Epic, Feature, Task, User story etc. Yhan lkhe sy ye isk sth associate ni hua hai this is just for documentation purpose
+        [WorkItem(1123)]
+        //ye multiple time askata hai
+        [TestCategory("BAT"), TestCategory("Critical"), TestCategory("Positive") ]
+        public void TestCase_001()
+        {
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://adactinhotelapp.com/";
+            driver.FindElement(By.Id("username")).SendKeys("Nabeel0075");
+            driver.FindElement(By.Name("password")).SendKeys("NabeelTester123");
+            driver.FindElement(By.ClassName("login_button")).Click();
+
+            driver.Close();
+
+        }
+        [TestMethod]
+        //Attributes
+        //assigning Owner (not more than 1)
+        [Owner("Usama")]
+        //Desciription
+        [Description("Test is demo Test")]
+        //WorkItem Different Tasks jo kisi kam k soorat me hon like Epic, Feature, Task, User story etc. Yhan lkhe sy ye isk sth associate ni hua hai this is just for documentation purpose
+        [WorkItem(1124)]
+        //ye multiple time askata hai
+        [TestCategory("BAT"), TestCategory("Critical"), TestCategory("Positive")]
+        public void TestCase_002()
+        {
+
+        }
+    }
+}
